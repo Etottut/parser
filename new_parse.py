2c4cf66d7d6307b86bdc5ec66d96eb5946c98f80
Alexandr Eliseev <shurmek1@gmail.com>
2021-07-29 10:44:07 +0300

    hotfix)

--- new_parse.py
+++ new_parse.py
@@ -1,23 +1,25 @@
 import requests
-from bs4 import BeautifulSoup
+import xlsxwriter
 import json
 
+workbook = xlsxwriter.Workbook('Output.xlsx')
 
 for i in range(0, 3000, 1000):
     page_number = int(i/1000)+1
     print(f"Начинаем парсить страницу {page_number}, offset = {i}")
     url = f"https://xn--b1aedfedwqbdfbnzkf0oe.xn--p1ai/api/v1/cms/publicorganizationpages/?fields=*&order=is_body_empty,title&limit=1000&offset={i}&search_id=1"
     r = requests.get(url=url)
+    data = json.loads(r.text)
 
-    soup = BeautifulSoup(r.text, 'lxml')
-    string = soup.find("body").text
+    worksheet = workbook.add_worksheet("{page_number}")
+    
+    row = 0
+    col = 0
 
-    json_string = json.dumps(string, indent=4)
-    # print(json_string)
+    for company in data["items"]:
+        worksheet.write(row, col, company["title"])
+        worksheet.write(row, col + 1, company["region"]["title"])
+        # add some columns
+        row += 1
 
-    with open(f"data_{page_number}.json", "w", encoding="utf-8") as file:
-        file.write(str(json_string))
-
-    data = json.loads(json_string)
-    with open(f"formatted_data_{page_number}.json", "w", encoding="utf-8") as file:
-        file.write(str(data))
+workbook.close()
